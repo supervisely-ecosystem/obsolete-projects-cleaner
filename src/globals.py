9eff03cbc12bc573e63b6f5e86bf9ed60f1b51a1
GoldenAnpu <seryios.sych@gmail.com>
2023-12-07 19:44:11 +0100

    fix None workspace id

--- src/globals.py
+++ src/globals.py
@@ -14,7 +14,10 @@ api = sly.Api.from_env()
 task_id = api.task_id
 age = int(os.environ.get("modal.state.age"))
 all_workspaces = bool(strtobool(os.environ.get("modal.state.allWorkspaces")))
-workspace_id = int(os.environ.get("modal.state.workspaceId"))
 sleep_days = int(os.environ.get("modal.state.sleep"))
 sleep_time = sleep_days * 86400
 stop = bool(strtobool(os.environ.get("modal.state.stop")))
+
+workspace_id = os.environ.get("modal.state.workspaceId")
+if workspace_id is not None:
+    workspace_id = int(workspace_id)
